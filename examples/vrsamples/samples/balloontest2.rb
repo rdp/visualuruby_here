a68260cf7076b6ac57e3e110b17fd055325cc641
rogerdpack <rogerpack2005@gmail.com>
2009-05-16 09:47:57 -0600

    subtle renames

--- examples/vrsamples/samples/balloontest2.rb
+++ examples/vrsamples/samples/balloontest2.rb
@@ -1,27 +1,38 @@
-#!/usr/bin/env ruby
+# slightly more complicated balloon
 
 require 'vr/vrcontrol'
 require 'vr/vrlayout'
 require 'vr/vrtray'
+require 'vr/vrtimer'
+require 'go' # ruby-wmi
+require 'pp'
 
 module MyForm
 
   include VRTrayiconFeasible
+  include VRTimerFeasible
 
   LoadIcon=    Win32API.new('user32', 'LoadIcon', 'II', 'I')
   INFO_ICON=   LoadIcon.call(0, 32516)
 
   def construct
-    @title= addControl(VREdit, 'title', 'Balloon title')
-    @text= addControl(VREdit, 'text', 'Balloon text')
-    @info= addControl(VRRadiobutton, 'info_rb', 'Info icon')
-    @info.check(true)
-    @warning= addControl(VRRadiobutton, 'warning_rb', 'Warning  icon')
-    @error= addControl(VRRadiobutton, 'error_rb', 'Error icon')
-    @none= addControl(VRRadiobutton, 'none_rb', 'No icon')
-    addControl(VRButton, 'show', 'Show balloon')
-    addControl(VRButton, 'exit', 'Exit')
-    create_trayicon(INFO_ICON, 'VRuby Balloon Tray Test') # last parameter is the tooltip. see traytest.rb
+    # create icon number 0
+    create_trayicon(INFO_ICON, 'VRuby Balloon Tooltip Test') # last parameter is the tooltip. see traytest.rb
+
+    self_timer
+    addTimer 1000
+  end
+
+  def self_timer
+   infoicon = VRTrayiconFeasible::NIIF_NONE
+
+   procs = WMI::Win32_Process.find(:all)
+   text = ""
+   for proc in procs.sort_by{|p| p.WorkingSetSize.to_i}.reverse[0..5]
+     text += "#{proc.Name}: #{proc.WorkingSetSize}\n"
+   end
+
+   modify_trayicon5(nil, 'title', text, infoicon, 0.01, 0)
   end
 
   def exit_clicked
@@ -43,6 +54,7 @@ module MyForm
 
   def self_trayballoonclicked iconid
     puts 'User clicked inside balloon!'
+    exit(0)
   end
 
   def show_clicked
@@ -55,7 +67,7 @@ module MyForm
         icon
       end
     end
-    modify_trayicon5(nil, @title.text, @text.text, infoicon)
+    modify_trayicon5(nil, @title.text, @text.text, infoicon, 0.01, 0)
   end
 
 end
@@ -65,8 +77,7 @@ frm.caption= 'Balloon Tooltip Test'
 frm.extend(MyForm)
 frm.extend(VRVertLayoutManager)
 frm.move(100, 100, 300, 200)
-frm.create.show
+frm.create
 VRLocalScreen.messageloop
 
 # see \vr\contrib\vrtrayballoon.rb for a more complete list of icons/messages 
-# note that if you create balloons with the same text and they *once* click on the x, it will not display again...something like that
